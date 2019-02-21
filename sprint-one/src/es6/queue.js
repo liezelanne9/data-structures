7bde9a1ef10f3be182be65582e6e2b10a06e1d07
liezelanne9 <liezelanne9@gmail.com>
2019-02-20 17:54:08 -0800

    sprint-one stack and queue (es6) - complete

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.i = 0;
+    this.j = 0;
   }
-
+  enqueue(value) {
+    this.storage[this.i] = value;
+    this.i++;
+  }
+  
+  dequeue() {
+    if (this.i > 0) {
+      var deleted = this.storage[0];
+      delete this.storage[0];
+      for (var key in this.storage) {
+        this.storage[this.j] = this.storage[key];
+        this.j++
+      }
+      this.i = this.i - 1;
+      delete this.storage[this.i];
+      this.j = 0;
+      return deleted;
+    }
+  }
+  
+  size() {
+    return this.i;
+  }  
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.i = 0;
   }
-
+  push(value) {
+    this.storage[this.i] = value;
+    this.i++;
+  }
+  
+  pop() {
+    if (this.i > 0) {
+      var deleted = this.storage[(this.i - 1)];
+      delete this.storage[this.i - 1];
+      this.i = this.i - 1;
+      return deleted;
+    }
+  }
+  
+  size() {
+    return this.i;
+  }
 }
