671bee3ae1507730b0fb5ab817ad332477f8a8ac
Liezel Manalo <liezelanne9@gmail.com>
2019-02-22 11:21:56 -0800

    tree.js - should correctly detect nested children

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,39 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+    
+  if (this.value === target) {
+    return true;
+  };
+  for (var i = 0; i < this.children.length; i++) {
+    console.log(this.children[i]);
+
+
+    if ((this.children[i]).value === target) {
+        return true;
+    } else {
+        return this.children[i].contains(target);
+    }
+  }
+  return false;
 };
 
 
