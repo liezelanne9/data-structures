4831a8cf2c853d3530ca6d56857db949e2fd98b8
Liezel Manalo <liezelanne9@gmail.com>
2019-07-17 15:40:59 -0700

    [sprint-one] Functional and Functional-Shared complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,19 +2,29 @@ const Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   const someInstance = {};
-
   _.extend(someInstance, queueMethods);
+  someInstance.storage = {};
+  someInstance.nextKey = 0;
+  return someInstance;
 };
 
-var queueMethods = {
-  push: function (value) {
-
+const queueMethods = {
+  enqueue: function (value) {
+    this.storage[this.nextKey] = value;
+    this.nextKey += 1;
   },
-  pop: function (value) {
-
+  dequeue: function (value) {
+    if (this.nextKey > 0) {
+      const popped = this.storage[0];
+      this.nextKey -= 1;
+      for (let i = 0; i < this.nextKey; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+      return popped;
+    }
   },
   size: function (value) {
-    
+    return this.nextKey;
   }
 };
 
